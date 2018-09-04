55c3dbf1b5a632743ad8100158157f0f53d864c8
LeachZhou <leachzhou@163.com>
2018-09-04 15:20:28 +0800

    添加壁纸

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    imageActive: '../../image/1.jpg',
-    imageList: [
-      '../../image/2.jpg',
-      '../../image/3.jpg',
-      '../../image/4.jpg'
+    mainActive: 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb5337111a_1080x1920.jpg.97.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+    mainList: [
+      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/1/2018/09/03/5b8cf3b710ac5_1080x1920.jpg.91.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb3b67e2e4_1080x1920.jpg.88.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb5b43eafe_1080x1920.jpg.81.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb3cf23ac8_1080x1920.jpg.50.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/04/5b8e073ccc625_1080x1920.jpg.95.webp?uid=f3412d529b990ef65f6f3b0425bec6f7'
     ],
     animationData: {},
     screenHeight: 0,
@@ -29,49 +31,62 @@ Page({
         });
       }
     });
+    // wx.request({
+    //   url: '',
+    //   method: 'GET',
+    //   header: {
+    //     'content-type': 'application/json'
+    //   },
+    //   success: function(res) {
+    //     console.log(res.data);
+    //   },
+    //   fail: function(res) {
+    //     console.log(res.data)
+    //   }
+    // })
   },
   movableChange: function(e) {
+    let _this = this;
     console.log(`X: ${e.detail.x}`);
     console.log(`Y: ${e.detail.y}`);
     let deg = 0; // 旋转角度
     deg = -e.detail.x / 3;
-    var animation = wx.createAnimation({
+    let animation = wx.createAnimation({
       transformOrigin: "50% 0",
       duration: 50,
       timingFunction: "ease",
       delay: 0
     })
-    this.animation = animation;
+    _this.animation = animation;
     animation.rotate(deg).step();
-    this.setData({
+    _this.setData({
       animationData: animation.export()
     })
-
   },
   movableEnd: function(e) {
+    let _this = this;
     console.log(`endX: ${e.changedTouches[0].pageX}`);
     console.log(`endY: ${e.changedTouches[0].pageY}`);
     let endX = e.changedTouches[0].pageX;
     let endY = e.changedTouches[0].pageY;
     if (endY < 300 && endX > 300) {
-      this.setData({
+      _this.setData({
         endOpacity: 0
       })
-      if (this.data.imageList.length) {
-        let imageActiveChange = this.data.imageList[0];
-        this.setData({
+      if (_this.data.mainList.length) {
+        let mainActive = _this.data.mainList.shift();
+        _this.setData({
           x: 0,
           y: 0,
-          imageActive: imageActiveChange,
+          mainActive: mainActive,
           endOpacity: 1
         })
-        this.data.imageList.shift();
-        this.setData({
-          imageList: this.data.imageList
+        _this.setData({
+          mainList: _this.data.mainList
         })
       }
     } else {
-      this.setData({
+      _this.setData({
         x: 0,
         y: 0
       })
