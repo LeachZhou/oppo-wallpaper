9fdfd9f740fcd80349ac3855b7cb3f70f42d7dc2
LeachZhou <leachzhou@163.com>
2018-09-06 15:38:47 +0800

    添加数据处理

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,20 @@
+var Bmob = require('../../utils/Bmob-1.6.3.min.js');
+var oppoJson = require('../../utils/oppoJson.js');
+var oppo20180906 = require('../../utils/data/oppo20180906.js');
+
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
     mainActive: 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb5337111a_1080x1920.jpg.97.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
     mainList: [
-      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/1/2018/09/03/5b8cf3b710ac5_1080x1920.jpg.91.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb3b67e2e4_1080x1920.jpg.88.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb5b43eafe_1080x1920.jpg.81.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/1/2018/09/03/5b8cf3b710ac5_1080x1920.jpg.91.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb3b67e2e4_1080x1920.jpg.88.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
+      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb5b43eafe_1080x1920.jpg.81.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
       'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb3cf23ac8_1080x1920.jpg.50.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/04/5b8e073ccc625_1080x1920.jpg.95.webp?uid=f3412d529b990ef65f6f3b0425bec6f7'
+      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/04/5b8e073ccc625_1080x1920.jpg.95.webp?uid=f3412d529b990ef65f6f3b0425bec6f7'
     ],
     animationData: {},
     screenHeight: 0,
@@ -31,29 +35,38 @@ Page({
         });
       }
     });
+
+
+    let b = oppoJson.oppoJson(oppo20180906.data);
+    console.log(b)
+
+
+
     // wx.request({
-    //   url: 'https://gitee.com/LeachZhou/Daily-Wallpaper/blob/master/20180903.json',
+    //   url: 'xxxxxxxxxxxxxx',
     //   method: 'GET',
     //   header: {
     //     'content-type': 'application/json'
     //   },
-    //   success: function(res) {
-    //     console.log(res.data);
+    //   success: function (res) {
+    //     console.log(res.data); 
     //   },
-    //   fail: function(res) {
+    //   fail: function (res) {
     //     console.log(res.data)
     //   }
     // })
+
+
   },
   movableChange: function(e) {
     let _this = this;
     console.log(`X: ${e.detail.x}`);
     console.log(`Y: ${e.detail.y}`);
     let deg = 0; // 旋转角度
-    deg = -e.detail.x / 3;
+    deg = e.detail.x / 3;
     let animation = wx.createAnimation({
-      transformOrigin: "50% 0",
-      duration: 50,
+      transformOrigin: "50% 100%",
+      duration: 200,
       timingFunction: "ease",
       delay: 0
     })
@@ -75,15 +88,35 @@ Page({
       })
       if (_this.data.mainList.length) {
         let mainActive = _this.data.mainList.shift();
-        _this.setData({
-          x: 0,
-          y: 0,
-          mainActive: mainActive,
-          endOpacity: 1
-        })
-        _this.setData({
-          mainList: _this.data.mainList
-        })
+        setTimeout(function() {
+          _this.setData({
+            mainActive: mainActive,
+            x: 0,
+            y: 0
+          })
+        }, 0)
+        setTimeout(function() {
+          _this.setData({
+            imgscale: 1 / 0.9,
+            imgY: 60,
+          })
+        }, 300)
+        setTimeout(function() {
+          _this.setData({
+            endOpacity: 1
+          })
+        }, 400)
+        setTimeout(function() {
+          _this.setData({
+            imgscale: 1,
+            imgY: 0,
+          })
+        }, 500)
+        setTimeout(function() {
+          _this.setData({
+            mainList: _this.data.mainList
+          })
+        }, 600)
       }
     } else {
       _this.setData({
@@ -91,8 +124,14 @@ Page({
         y: 0
       })
     }
+  },
+  previewImg: function() {
+    let arr = [];
+    arr.push(this.data.mainActive);
+    wx.previewImage({
+      urls: arr // 需要预览的图片http链接列表
+    })
   }
-
 
 })
 
