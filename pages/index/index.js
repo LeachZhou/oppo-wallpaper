4b45f961c35e1f5521abc050b3caae0eca023b21
LeachZhou <leachzhou@163.com>
2018-09-06 18:05:08 +0800

    添加标题坐着

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,21 +8,17 @@ const app = getApp();
 
 Page({
   data: {
-    mainActive: 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb5337111a_1080x1920.jpg.97.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-    mainList: [
-      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/1/2018/09/03/5b8cf3b710ac5_1080x1920.jpg.91.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb3b67e2e4_1080x1920.jpg.88.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb5b43eafe_1080x1920.jpg.81.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-      'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/03/5b8cb3cf23ac8_1080x1920.jpg.50.webp?uid=f3412d529b990ef65f6f3b0425bec6f7',
-      // 'https://magzinefs4.nearme.com.cn/uploads/wallpaper/2/2018/09/04/5b8e073ccc625_1080x1920.jpg.95.webp?uid=f3412d529b990ef65f6f3b0425bec6f7'
-    ],
+    mainActive: {},
+    mainList: [],
+    allResList: [],
     animationData: {},
     screenHeight: 0,
     screenWidth: 0,
     x: 0,
     y: 0,
     deg: 0,
-    endOpacity: 1
+    endOpacity: 1,
+    titleOpacity: 1
   },
   onLoad: function() {
     //获取屏幕宽高
@@ -35,12 +31,24 @@ Page({
         });
       }
     });
-
-
-    let b = oppoJson.oppoJson(oppo20180906.data);
-    console.log(b)
-
-
+  },
+  onReady: function() {
+    let _this = this;
+    _this.setData({
+      allResList: oppoJson.oppoJson(oppo20180906.data)
+    });
+    let mainListTemp = [];
+    for (let i = 0; i < 4; i++) {
+      mainListTemp.push(_this.data.allResList[i]);
+    }
+    for (let i = 0; i < 4; i++) {
+      _this.data.allResList.shift();
+    }
+    let mainActiveTemp = mainListTemp.shift();
+    _this.setData({
+      mainActive: mainActiveTemp,
+      mainList: mainListTemp
+    });
 
     // wx.request({
     //   url: 'xxxxxxxxxxxxxx',
@@ -55,31 +63,38 @@ Page({
     //     console.log(res.data)
     //   }
     // })
-
-
   },
   movableChange: function(e) {
-    let _this = this;
-    console.log(`X: ${e.detail.x}`);
-    console.log(`Y: ${e.detail.y}`);
+    let _this = this; 
+    // console.log(`X: ${e.detail.x}`);
+    // console.log(`Y: ${e.detail.y}`);
     let deg = 0; // 旋转角度
-    deg = e.detail.x / 3;
+    deg = e.detail.x / 2.5;
     let animation = wx.createAnimation({
       transformOrigin: "50% 100%",
       duration: 200,
       timingFunction: "ease",
       delay: 0
     })
-    _this.animation = animation;
+    // _this.animation = animation;
     animation.rotate(deg).step();
     _this.setData({
       animationData: animation.export()
     })
+    let titleOpacity;
+    if (e.detail.x == 0 && e.detail.y == 0) {
+      titleOpacity = 1;
+    } else {
+      titleOpacity = 0;
+    }
+    _this.setData({
+      titleOpacity: titleOpacity
+    })
   },
   movableEnd: function(e) {
     let _this = this;
-    console.log(`endX: ${e.changedTouches[0].pageX}`);
-    console.log(`endY: ${e.changedTouches[0].pageY}`);
+    // console.log(`endX: ${e.changedTouches[0].pageX}`);
+    // console.log(`endY: ${e.changedTouches[0].pageY}`);
     let endX = e.changedTouches[0].pageX;
     let endY = e.changedTouches[0].pageY;
     if (endY < 300 && endX > 300) {
@@ -88,35 +103,35 @@ Page({
       })
       if (_this.data.mainList.length) {
         let mainActive = _this.data.mainList.shift();
-        setTimeout(function() {
+        requestAnimationFrame(function() {
           _this.setData({
             mainActive: mainActive,
             x: 0,
             y: 0
           })
-        }, 0)
-        setTimeout(function() {
+        })
+        requestAnimationFrame(function() {
           _this.setData({
             imgscale: 1 / 0.9,
             imgY: 60,
           })
-        }, 300)
-        setTimeout(function() {
+        })
+        requestAnimationFrame(function() {
           _this.setData({
             endOpacity: 1
           })
-        }, 400)
-        setTimeout(function() {
+        })
+        requestAnimationFrame(function() {
           _this.setData({
             imgscale: 1,
             imgY: 0,
           })
-        }, 500)
-        setTimeout(function() {
+        })
+        requestAnimationFrame(function() {
           _this.setData({
             mainList: _this.data.mainList
           })
-        }, 600)
+        })
       }
     } else {
       _this.setData({
@@ -126,8 +141,9 @@ Page({
     }
   },
   previewImg: function() {
+    let _this = this;
     let arr = [];
-    arr.push(this.data.mainActive);
+    arr.push(_this.data.mainActive.img);
     wx.previewImage({
       urls: arr // 需要预览的图片http链接列表
     })
@@ -137,3 +153,4 @@ Page({
 
 //http://www.htmleaf.com/Demo/20141023275.html 洗牌图片切换特效
 //http://www.htmleaf.com/Demo/20141019230.html 图片堆叠效果
+//https://camo.githubusercontent.com/ffd745dc2e0e3f55b31b6e109c3869e58471b36e/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f313737383634322f73637265656e73686f74732f333832333930302f5f5f5f2d3131312e676966
