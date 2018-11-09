f378cdda3360a3cdb1263855c2dd1e5ca1d1941a
leachzhou <leachzhou@163.com>
2018-11-09 10:40:28 +0800

    修复了首页后一天显示错误及其他一些BUG

--- utils/bmob-info.js
+++ utils/bmob-info.js
@@ -36,27 +36,7 @@ module.exports = {
         for (let item of res) {
           arr.push(oppoJson.oppoJson(item.wallpaperData));
         }
-        let flatArr = util.flatten(arr);
-        for (let i = 0; i < flatArr.length; i++) {
-          if (flatArr[i].author.match('undefine') || flatArr[i].img.match('undefine')) { //如果作者或者图片里面有undefined，则删除
-            flatArr.splice(i, 1);
-          } else {
-            if (flatArr[i].title.match('R')) { //如果标题里面包含“R"
-              let index = flatArr[i].title.indexOf('R'); //获取R的位置
-              if (flatArr[i].title[index + 2]) { //R往后是否有2个字符的位置
-                // console.log(flatArr[i].title[index] + flatArr[i].title[index + 1]);
-                // if ((flatArr[i].title[index + 2]).match(/[\u4e00-\u9fa5]/)) { //往后的2个位置是否是汉字
-                let str = flatArr[i].title.substr(0, index) + ' ' + flatArr[i].title.substr(index + 2); //如果是汉字把R和R后面的一个字符删掉，前后进行拼接
-                flatArr[i].title = str;
-                // }
-              }
-              if (flatArr[i].title[flatArr[i].title.length - 1] == 'R') {
-                flatArr[i].title = flatArr[i].title.substr(0, index - 1);
-              }
-            }
-          }
-        }
-        fn(flatArr);
+        _this.wallResSplice(arr, fn);
       }).catch((res) => {
         console.log(res)
         wx.showToast({
@@ -79,45 +59,60 @@ module.exports = {
     })
   },
   /**
+   * 删除包含错误字符的数据
+   */
+  wallResSplice(arr,fn){
+    let _this = this;
+    let flatArr = util.flatten(arr);
+    for (let i = 0; i < flatArr.length; i++) {
+      if (flatArr[i].author.includes('undefine') || flatArr[i].img.includes('undefine')) { //如果作者或者图片里面有undefined，则删除
+        flatArr.splice(i, 1);
+        i = i - 1;//修改一下重点splice部分的代码，原理是我们缩减索引值，来达到和要删除元素的索引对上的目的。
+      } else {
+        if (flatArr[i].title.includes('R')) { //如果标题里面包含“R"
+          let index = flatArr[i].title.indexOf('R'); //获取R的位置
+          if (flatArr[i].title[index + 2]) { //R往后是否有2个字符的位置
+            // console.log(flatArr[i].title[index] + flatArr[i].title[index + 1]);
+            // if ((flatArr[i].title[index + 2]).match(/[\u4e00-\u9fa5]/)) { //往后的2个位置是否是汉字
+            let str = flatArr[i].title.substr(0, index) + ' ' + flatArr[i].title.substr(index + 2); //如果是汉字把R和R后面的一个字符删掉，前后进行拼接
+            flatArr[i].title = str;
+            // }
+          }
+          if (flatArr[i].title[flatArr[i].title.length - 1] == 'R') {
+            flatArr[i].title = flatArr[i].title.substr(0, index - 1);
+          }
+        }
+      }
+    }
+    fn(flatArr);
+  },
+  /**
    * 上传资源请求数据 从2018-11-07开始
    */
   uploadWallResAna(fn, day) {
     let _this = this;
-    _this.uploadWallRes((rest) => {
-      for (let item of rest) {
+    _this.uploadWallRes((res) => {
+      if (res.length) {
         wx.request({
-          url: item.file.url,
+          url: 'https' + res[0].file.url.match('http').input.split('http')[1],
           header: {
             'content-type': 'application/json' // 默认值
           },
           success(ret) {
             let arr = [];
             arr.push(_this.oppoJson(ret.data));
-            let flatArr = util.flatten(arr);
-            for (let i = 0; i < flatArr.length; i++) {
-              if (flatArr[i].author.match('undefine') || flatArr[i].img.match('undefine')) { //如果作者或者图片里面有undefined，则删除
-                flatArr.splice(i, 1);
-              } else {
-                if (flatArr[i].title.match('R')) { //如果标题里面包含“R"
-                  let index = flatArr[i].title.indexOf('R'); //获取R的位置
-                  if (flatArr[i].title[index + 2]) { //R往后是否有2个字符的位置
-                    // console.log(flatArr[i].title[index] + flatArr[i].title[index + 1]);
-                    // if ((flatArr[i].title[index + 2]).match(/[\u4e00-\u9fa5]/)) { //往后的2个位置是否是汉字
-                    let str = flatArr[i].title.substr(0, index) + ' ' + flatArr[i].title.substr(index + 2); //如果是汉字把R和R后面的一个字符删掉，前后进行拼接
-                    flatArr[i].title = str;
-                    // }
-                  }
-                  if (flatArr[i].title[flatArr[i].title.length - 1] == 'R') {
-                    flatArr[i].title = flatArr[i].title.substr(0, index - 1);
-                  }
-                }
-              }
-            }
-            fn(flatArr);
+            _this.wallResSplice(arr,fn);
           }
         })
+      } else {
+        wx.showToast({
+          title: '总是空空如也~',
+          image: '../../image/blank.png',
+          icon: 'none',
+          mask: true,
+          duration: 2000
+        })
       }
-
     }, day);
   },
   /**
@@ -141,8 +136,10 @@ module.exports = {
       query.equalTo("time", "<", nextTime);
       query.equalTo("time", ">", nowTime);
       query.find().then((res) => {
+        // console.log(res)
         fn(res);
       }).catch((res) => {
+        console.log(res)
         wx.showToast({
           title: '请求失败',
           image: '../../image/err.png',
@@ -227,27 +224,7 @@ module.exports = {
       for (let item of res) {
         arr.push(oppoJson.oppoJson(item.wallpaperData));
       }
-      let flatArr = util.flatten(arr);
-      for (let i = 0; i < flatArr.length; i++) {
-        if (flatArr[i].author.match('undefine') || flatArr[i].img.match('undefine')) { //如果作者或者图片里面有undefined，则删除
-          flatArr.splice(i, 1);
-        } else {
-          if (flatArr[i].title.match('R')) { //如果标题里面包含“R"
-            let index = flatArr[i].title.indexOf('R'); //获取R的位置
-            if (flatArr[i].title[index + 2]) { //R往后是否有2个字符的位置
-              // console.log(flatArr[i].title[index] + flatArr[i].title[index + 1]);
-              // if ((flatArr[i].title[index + 2]).match(/[\u4e00-\u9fa5]/)) { //往后的2个位置是否是汉字
-              let str = flatArr[i].title.substr(0, index) + ' ' + flatArr[i].title.substr(index + 2); //如果是汉字把R和R后面的一个字符删掉，前后进行拼接
-              flatArr[i].title = str;
-              // }
-            }
-            if (flatArr[i].title[flatArr[i].title.length - 1] == 'R') {
-              flatArr[i].title = flatArr[i].title.substr(0, index - 1);
-            }
-          }
-        }
-      }
-      fn(flatArr);
+      _this.wallResSplice(arr, fn);
     }).catch((res) => {
       console.log(res)
       wx.showToast({
@@ -292,39 +269,17 @@ module.exports = {
   newDateSelect(fn, day) {
     let _this = this;
     _this.newUploadWallRes((res) => {
-      for (let item of res) {
-        wx.request({
-          url: item.file.url,
-          header: {
-            'content-type': 'application/json' // 默认值
-          },
-          success(ret) {
-            let arr = [];
-            arr.push(_this.oppoJson(ret.data));
-            let flatArr = util.flatten(arr);
-            for (let i = 0; i < flatArr.length; i++) {
-              if (flatArr[i].author.match('undefine') || flatArr[i].img.match('undefine')) { //如果作者或者图片里面有undefined，则删除
-                flatArr.splice(i, 1);
-              } else {
-                if (flatArr[i].title.match('R')) { //如果标题里面包含“R"
-                  let index = flatArr[i].title.indexOf('R'); //获取R的位置
-                  if (flatArr[i].title[index + 2]) { //R往后是否有2个字符的位置
-                    // console.log(flatArr[i].title[index] + flatArr[i].title[index + 1]);
-                    // if ((flatArr[i].title[index + 2]).match(/[\u4e00-\u9fa5]/)) { //往后的2个位置是否是汉字
-                    let str = flatArr[i].title.substr(0, index) + ' ' + flatArr[i].title.substr(index + 2); //如果是汉字把R和R后面的一个字符删掉，前后进行拼接
-                    flatArr[i].title = str;
-                    // }
-                  }
-                  if (flatArr[i].title[flatArr[i].title.length - 1] == 'R') {
-                    flatArr[i].title = flatArr[i].title.substr(0, index - 1);
-                  }
-                }
-              }
-            }
-            fn(flatArr);
-          }
-        })
-      }
+      wx.request({
+        url: 'https' + res[0].file.url.match('http').input.split('http')[1],
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success(ret) {
+          let arr = [];
+          arr.push(_this.oppoJson(ret.data));
+          _this.wallResSplice(arr, fn);
+        }
+      })
     }, day)
   },
   /**
